a2ec9e4c920455656ec5d4dd7a8d70296edb2f9b
Christos <chris.paraskeva10@gmail.com>
2017-02-27 17:32:13 +0000

    added cat image with border to app.rb file

--- app.rb
+++ app.rb
@@ -11,3 +11,7 @@ end
 get '/secret/bug' do
   "There's a spider on your face, Chris. Look! Hello? "
 end
+
+get '/cat' do
+   "<img src=http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png style='border:3px dotted red'>"
+end
