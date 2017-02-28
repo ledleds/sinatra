0ba9739c02eac4cd18c153ace416853b447eee2f
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-02-28 13:49:19 +0000

    New cat_form erb created

--- app.rb
+++ app.rb
@@ -17,8 +17,11 @@ get '/random-cat' do
    erb(:index)
 end
 
-get '/named-cat' do
-  p params
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   @cat_name = params[:name]
   erb(:index)
 end
